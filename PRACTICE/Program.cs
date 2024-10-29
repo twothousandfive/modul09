f96542d73f810d840d82cf8e169a898efd3d06da
Almazz <050606500373@stud.satbayev.university>
2024-10-29 19:24:18 +0500

    modul09

---
+++ PRACTICE/Program.cs
@@ -0,0 +1,16 @@
+﻿using PRACTICE.ЗАДАЧА2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DeliveryServiceFactory factory = new DeliveryServiceFactory();
+
+        // Использование внутренней службы
+        IInternalDeliveryService internalService = factory.GetDeliveryService("internal");
+        internalService.DeliverOrder("123");
+
+        // Использование внешней службы A
+        IInternalDeliveryService externalServiceA = factory.GetDeliveryService("externalA");
+        externalServiceA.DeliverOrder("456");
+    }
+}
